9caeb2a5892a0fd3639bfc462eaa03627c4ecca8
Shaun <sfox@snaptrends.com>
2019-05-02 20:08:53 -0500

    dealing with z-index

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -13,24 +13,22 @@ $("#openMenu").on("click", function() {
 	$("#navigation").toggleClass("nav-open");
 	$("#openMenu").addClass("hamburger-hidden");
   $("#closeMenu").addClass("close-shown");
-  $(".post-nav").hide();
+  $(".post-nav-wrapper").hide();
 });
 
 $("#closeMenu").on("click", function() {
   $("#navigation").toggleClass("nav-open");
   $("#openMenu").removeClass("hamburger-hidden");
   $("#closeMenu").removeClass("close-shown");
-  $(".post-nav").show();
+  $(".post-nav-wrapper").show();
 });
 
 // Arrow through image pages
 $(document).keydown(function(e){
   if (e.keyCode === 37) { // left arrow
-     // alert( "left pressed" );
      $("#previousClick").get(0).click();
      console.log("previous-click");
   } else if (e.keyCode === 39) { // right arrow
-     // alert( "right pressed" );
      $("#nextClick").get(0).click();
      console.log("next-click");
   } else {
